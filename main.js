88642aa6e6935fcc0cd894dd178d8bc7f6df2032
Tyler1551 <46712398+Tyler1551@users.noreply.github.com>
2020-04-17 02:34:58 +0100

    added functional search

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function checkinput(input){
+  var search_form = document.getElementById('search-form')
+  var content = document.getElementById('content')
+  var len = input.length;
+
+  if(input.includes(":r")){
+    var search = input.slice(4,len)
+    content.value = search
+    content.placeholder = "reddit search"
+    search_form.action = "https://www.reddit.com/search/"
+  }
+  if(input.includes(":y")){
+    var search = input.slice(4,len)
+    content.value = search
+    content.placeholder = "youtube search"
+    search_form.action = "https://www.youtube.com/search"
+  }
+  if(input.includes(":g")){
+    var search = input.slice(4,len)
+    content.value = search
+    content.placeholder = "google search"
+    search_form.action = "https://www.google.co.uk/search"
+  }
+}
