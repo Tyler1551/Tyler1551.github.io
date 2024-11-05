74ddaad71946dc6fb04fd19d3e0a2686d73c3523
Tyler1551 <kakuja16@gmail.com>
2024-11-05 17:50:53 +0000

    Added commands in the search bar e.g /a /g /y for amazon, google and youtube

--- public/js/search.js
+++ public/js/search.js
@@ -1,23 +1,62 @@
 
-function checkSearch(){
-    var type = document.getElementById('search_option');
+// function checkSearch(){
+//     var type = document.getElementById('search_option');
+//     var search = document.getElementById('searchbar');
+//     var inputBar = document.getElementById('inputbar');
+//     console.log(type.value)
+    
+    
+//     switch(type.value){
+//         case "YouTube":
+//             search.action = "https://www.youtube.com/results?search_query=";
+//             inputBar.name = "q"
+//             break;
+//         case "Amazon": 
+//             search.action = "https://www.amazon.co.uk/s?k=";
+//             inputBar.name = "k"
+//             break;
+//         default:
+//             search.action = "https://google.co.uk/search?q="
+//             inputBar.name = "q"
+//     }
+// }
+
+
+
+function keyWord(){
+    var searchBar = document.getElementById('inputbar')
+    var type = searchBar.value.slice(0, 2)
+
     var search = document.getElementById('searchbar');
-    var inputBar = document.getElementById('inputbar')
-    console.log(type.value)
-    
-    
-    switch(type.value){
-        case "YouTube":
-            search.action = "https://www.youtube.com/results?search_query=";
-            inputBar.name = "q"
+
+    var newSearch = searchBar.value.slice(3)
+
+    console.log("KEYWORD FUNCTION: TYPE = " + type)
+    console.log("KEYWORD FUNCTION: newSearch = " + newSearch)
+    switch(searchBar.value.slice(0, 2)){
+        case "/g":
+            searchBar.value = newSearch
+            search.action = "https://google.co.uk/search?q="
+            searchBar.name = "q"
             break;
-        case "Amazon": 
+
+        case "/a": 
+            searchBar.value = newSearch
             search.action = "https://www.amazon.co.uk/s?k=";
-            inputBar.name = "k"
+            searchBar.name = "k"
+            
             break;
+
+        case "/y":
+            searchBar.value = newSearch
+            search.action = "https://www.youtube.com/results?search_query=";
+            searchBar.name = "q"
+            break;
+
         default:
             search.action = "https://google.co.uk/search?q="
-            inputBar.name = "q"
+            searchBar.name = "q"
+
     }
 }
 
