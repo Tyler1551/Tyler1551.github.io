97dbe3eda4bb84ebd7d35b3ed643365bec367962
Tyler1551 <Tylerpower1551@gmail.com>
2025-09-30 09:23:49 +0100

    adding space from the sunset time and temp

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,23 +1,28 @@
 let lat, long
-function getWeather(){
-    if("geolocation" in navigator){
-        navigator.geolocation.getCurrentPosition((position) =>{
+function getWeather() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
             lat = position.coords.latitude;
             long = position.coords.longitude;
 
-            var api = 'https://api.open-meteo.com/v1/forecast?latitude='+lat+'&longitude=' + long + '&hourly=temperature_2m&current_weather=true&daily=sunset';
+            var api = 'https://api.open-meteo.com/v1/forecast?latitude=' + lat + '&longitude=' + long + '&hourly=temperature_2m&current_weather=true&daily=sunset,sunrise';
             var req = new XMLHttpRequest();
-    
+
             req.open('GET', api, true);
-    
-            req.onload = function(){
+
+            req.onload = function () {
                 var data = JSON.parse(this.response);
-    
+
                 var temperature = data.current_weather.temperature;
                 var weatherCode = data.current_weather.weathercode;
 
+                var sunrise = data.daily.sunrise[0].split('T')[1];
+                var sunset = data.daily.sunset[0].split('T')[1];
+
+
+                console.log(sunrise, sunset);
                 document.getElementById('sunset').innerText = data.daily.sunset[0].split('T')[1]
-    
+
                 // weather codes are what im going to use for icons. Choose what's appropriate from list
                 /* 
                     WMO Weather interpretation codes (WW)
@@ -35,35 +40,35 @@ function getWeather(){
                     85, 86	Snow showers slight and heavy
                     95 *	Thunderstorm: Slight or moderate
                     96, 99 *	Thunderstorm with slight and heavy hail
-                */                
-                    
-                    if(weatherCode <= 2){
-                        document.getElementById('icon').data = 'public/ico/weather/sun.svg';
-                    }
-                    else if(weatherCode <= 3){
-                        document.getElementById('icon').data = 'public/ico/weather/cloud.svg';
-                    }
-                    else if(weatherCode >= 51 && weatherCode <= 55){
-                        document.getElementById('icon').data = 'public/ico/weather/cloud-drizzle.svg';
-                    }
-                    else if(weatherCode >= 61 && weatherCode <= 65){
-                        document.getElementById('icon').data = 'public/ico/weather/cloud-rain.svg';
-                    }
-                    else if(weatherCode >= 71 && weatherCode <= 75){
-                        document.getElementById('icon').data = 'public/ico/weather/cloud-snow.svg';
-                    }
-                    else if(weatherCode >= 80 && weatherCode <= 82){
-                        document.getElementById('icon').data = 'public/ico/weather/umbrella.svg';
-                    }
-                    else if(weatherCode >= 95){
-                        document.getElementById('icon').data = 'public/ico/weather/cloud-lightning.svg';
-                    }
-                    document.getElementById('temp').innerHTML = temperature + '°C';
+                */
+
+                if (weatherCode <= 2) {
+                    document.getElementById('icon').data = 'public/ico/weather/sun.svg';
+                }
+                else if (weatherCode <= 3) {
+                    document.getElementById('icon').data = 'public/ico/weather/cloud.svg';
+                }
+                else if (weatherCode >= 51 && weatherCode <= 55) {
+                    document.getElementById('icon').data = 'public/ico/weather/cloud-drizzle.svg';
+                }
+                else if (weatherCode >= 61 && weatherCode <= 65) {
+                    document.getElementById('icon').data = 'public/ico/weather/cloud-rain.svg';
+                }
+                else if (weatherCode >= 71 && weatherCode <= 75) {
+                    document.getElementById('icon').data = 'public/ico/weather/cloud-snow.svg';
+                }
+                else if (weatherCode >= 80 && weatherCode <= 82) {
+                    document.getElementById('icon').data = 'public/ico/weather/umbrella.svg';
+                }
+                else if (weatherCode >= 95) {
+                    document.getElementById('icon').data = 'public/ico/weather/cloud-lightning.svg';
+                }
+                document.getElementById('temp').innerHTML = temperature + '°C';
             }
             req.send();
         });
     }
-    else{
+    else {
         console.log('No geolocation data available, weather will be disabled');
     }
 }
